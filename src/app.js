798534228be53849e7a2d69fbf98b6e7fea58197
Peter Tountas <petertountas1@gmail.com>
2017-06-21 21:16:02 -0500

    more adds

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 import {createStore} from 'redux';
 
 // STEP 3 DEFINE REDUCERS
-const reducer = function(state=0,action) {
+const reducer = function(state={books: []},action) {
     switch (action.type){
-        case "INCREMENT":
-        return state + action.payload;
+        case "POST_BOOK":
+        let books = state.books.concat(action.payload);
+        return {books};
         break;
     }
     return state
@@ -15,9 +16,37 @@ const reducer = function(state=0,action) {
 const store = createStore(reducer);
 
 store.subscribe(function(){
-    console.log("the state is: " + store.getState());
+    console.log("the state is: " , store.getState());
+    console.log("the price is: " , store.getState().price);
+  //  console.log("the price is: " , store.getState()[1].price);
 })
 
 
 // STEP 2 create and dispatch actions
-store.dispatch({ type:"INCREMENT", payload: 1})
+store.dispatch({ type:"POST_BOOK",
+                 payload: [{
+                    id: 1,
+                    title: "this is the book title",
+                    description: 'this is the book description',
+                    price: 5.00 
+                 },
+                 {
+                    id: 2,
+                    title: "this is the 2nd book title",
+                    description: 'this is the second book description',
+                    price: 10.00 
+                 }
+                 ]
+            });
+
+
+//DISPATCH A SECOND ACTION; 
+store.dispatch({
+    type: "POST_BOOK",
+    payload: {
+        id: 3,
+        title: "3rd book title",
+        description: 'this is the 3rd book title',
+        price: 33.33
+    }
+});
